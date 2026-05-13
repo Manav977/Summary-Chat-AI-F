9624274554ea38eeabc56e4166eda5b16d85e4cd
manav <manavnasra8080@gmail.com>
2026-05-13 20:50:36 +0530

    Add all frontend files and components

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx';
 import { AppProvider } from './context/AppContext.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
 import { ChatProvider } from './context/ChatContext.jsx';
-import './styles/global.css'
+import './styles/index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
